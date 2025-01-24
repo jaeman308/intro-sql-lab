767b73faacbc962d7d2220e5be8e51b715894bea
jborruel308@me.com <jborruel308@me.com>
2025-01-23 19:22:47 -0800

    final code

--- clues.sql
+++ clues.sql
@@ -3,31 +3,90 @@
 -- Write SQL query here
 
 
+ Holy See (Vatican City State) |       1000
+
+SELECT name, population
+FROM countries
+WHERE region = 'Southern Europe'
+ORDER BY population ASC;
+
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
 -- Write SQL query here
 
+ Italian
+
+SELECT language
+FROM countrylanguages
+WHERE countrycode = 'VAT' AND isofficial = TRUE;
+
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
+             name              | code | language 
+-------------------------------+------+----------
+ Italy                         | ITA  | Italian
+ Italy                         | ITA  | Italian
+ Italy                         | ITA  | Italian
+ Italy                         | ITA  | Italian
+ Italy                         | ITA  | Italian
+ San Marino                    | SMR  | Italian
+ San Marino                    | SMR  | Italian
+ San Marino                    | SMR  | Italian
+ San Marino                    | SMR  | Italian
+ San Marino                    | SMR  | Italian
+ Holy See (Vatican City State) | VAT  | Italian
+ Holy See (Vatican City State) | VAT  | Italian
+ Holy See (Vatican City State) | VAT  | Italian
+ Holy See (Vatican City State) | VAT  | Italian
+ Holy See (Vatican City State) | VAT  | Italian
 
 -- Write SQL query here
+SELECT countries.name, countries.code, countrylanguages.language
+FROM countries 
+JOIN countrylanguages ON countries.code = countrylanguages.countrycode
+WHERE countrylanguages.isofficial = TRUE
+  AND countries.region = 'Southern Europe'
+  AND countrylanguages.language = 'Italian';
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
-
+    name    
+------------
+ Serravalle
+ Serravalle
+ Serravalle
+ Serravalle
+ Serravalle
 -- Write SQL query here
-
+SELECT name
+FROM cities
+WHERE countrycode = 'SMR'  
+  AND name != 'San Marino';
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
-
+ city  | country 
+-------+---------
+ Serra | Brazil
 -- Write SQL query here
+SELECT cities.name AS city, countries.name AS country
+FROM cities 
+JOIN countries ON cities.countrycode = countries.code
+WHERE cities.name = 'Serra'
+  AND countries.continent = 'South America';
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
-
+ capital  
+----------
+ Brasília
 -- Write SQL query here
+
+SELECT cities.name AS capital
+FROM cities 
+JOIN countries  ON cities.id = countries.capital
+WHERE countries.name = 'Brazil';
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
@@ -42,3 +101,9 @@
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+SELECT name,  population FROM cities WHERE population = 91084;
+
+
+     name     | population 
+--------------+------------
+ Santa Monica |      91084
